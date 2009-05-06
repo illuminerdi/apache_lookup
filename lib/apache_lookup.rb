9337f6d007484f5db2809d713092baaee2180124
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-05-05 22:40:52 -0700

    started fleshing out tests and getting the code working with it. flexmock to the rescue to provide internet-less testing of methods that use Resolv.

--- lib/apache_lookup.rb
+++ lib/apache_lookup.rb
@@ -1,3 +1,47 @@
-class Apachelookup
+#!/usr/bin/env ruby -w
+
+require 'optparse'
+require 'resolv'
+
+class ApacheLookup
   VERSION = '0.0.1'
+  
+  attr_accessor :options, :log_data
+  
+  def initialize(args=[])
+    self.options = {:threads => 5}
+    
+    OptionParser.new do |opts|
+      opts.banner = "Usage: apache_lookup [-t] NUM_THREADS log_file.log"
+      
+      opts.separator ""
+      opts.separator "Specific options:"
+
+      opts.on("-t NUM_THREADS", "Maximum number of threads to use for parsing") do |t|
+        self.options[:threads] = t.to_i if t.to_i > 0
+        
+      end
+      opts.on("-h", "This information") do |h|
+        puts opts
+        exit 0
+      end
+    end.parse!(args)
+        
+    load_log args.shift
+  end
+  
+  def load_log file=""
+    raise ApacheLookupError, "No log file supplied" if(file.nil? or file == "")
+  end
+  
+  def lookup log_line=""
+    log_bits = log_line.split('-')
+    dns = Resolv.getname(log_bits[0].strip)
+    log_bits[0] = "#{dns} "
+    log_bits.join('-')
+  end
 end
+
+class ApacheLookupError < Exception; end
+
+ApacheLookup.new(ARGV) if __FILE__ == $0
--- test/test_apache_lookup.rb
+++ test/test_apache_lookup.rb
@@ -1,32 +1,50 @@
 require 'test/unit'
 require 'apache_lookup'
+require 'fileutils'
 require 'rubygems'
 require 'flexmock/test_unit'
 
 class TestApacheLookup < Test::Unit::TestCase
 
   def setup
-    File.delete('test_my_logs.log') if File.exists?('test_my_logs.log')
-    File.delete('test_my_logs.log.orig') if File.exists?('test_my_logs.log.orig')
+    @dir = "#{FileUtils.pwd}/test"
     # should make a local my_logs.log to work with so future runs of tests don't get clobbered by the last run
-    
+    FileUtils.cp("#{@dir}/my_logs.log", "#{@dir}/test_my_logs.log")
+
     # should delete the cache log if it exists to not clobber future tests.
+    File.delete("#{@dir}/lookup_cache.log") if File.exists?("#{@dir}/lookup_cache.log")
+  end
+  
+  def teardown
+    File.delete("#{@dir}/test_my_logs.log") if File.exists?("#{@dir}/test_my_logs.log")
+    File.delete("#{@dir}/test_my_logs.log.orig") if File.exists?("#{@dir}/test_my_logs.log.orig")
   end
 
   def test_handles_no_log_file
-    flunk "should fail to user if no log file is given."
+    assert_raise ApacheLookupError do
+      ApacheLookup.new()
+    end
   end
-
-  def test_inappropriate_extra_arg
-    flunk "for when a user provides something other than -t, a number, and a log file"
+  
+  def test_loads_log_file
+    flunk "must make sure it is able to read in the file"
   end
 
   def test_bad_threadcount_value
-    flunk "for when a user specifies a number of threads, but does so badly: '1s,4!'"
+    # "for when a user specifies a number of threads, but does so badly: 'foo'"
+    al = ApacheLookup.new(%w(-t foo my_logs.log))
+    assert_equal 5, al.options[:threads]
   end
 
   def test_resolves_domain_name
-    flunk "use flexmock to capture rsolv's attempt and return 'example.com' or something."
+    # "use flexmock to capture rsolv's attempt and return 'example.com' or something."
+    ip_address = "75.119.201.189"
+    dns = "www.foo.com"
+    al = ApacheLookup.new(["#{@dir}/test_my_logs.log"])
+    flexmock(Resolv).should_receive(:getname).with(ip_address).once.and_return(dns)
+    actual = al.lookup('75.119.201.189 - - [29/Apr/2009:16:07:44 -0700] "GET /favicon.ico HTTP/1.1" 200 1406')
+    expected = 'www.foo.com - - [29/Apr/2009:16:07:44 -0700] "GET /favicon.ico HTTP/1.1" 200 1406'
+    assert_equal actual, expected
   end
 
   def test_caches_domain_name
