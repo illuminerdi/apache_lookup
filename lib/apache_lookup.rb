0da9c43141ebee6c6b51e971b0b0e1319b13decd
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-05-07 20:35:42 -0700

    finished clearing up tests. just have threads left to do.

--- lib/apache_lookup.rb
+++ lib/apache_lookup.rb
@@ -2,11 +2,16 @@
 
 require 'optparse'
 require 'resolv'
+require 'fileutils'
+require 'time'
 
 class ApacheLookup
   VERSION = '0.0.1'
+  CACHE_DECAY = 86400
 
-  attr_accessor :options, :log_data
+  attr_accessor :options, :log_data, :cache_file, :orig_file
+
+  CACHE_FILE = File.dirname(__FILE__) + "/lookup_cache.txt"
 
   def initialize(args=[])
     self.options = {:threads => 5}
@@ -19,29 +24,72 @@ class ApacheLookup
 
       opts.on("-t NUM_THREADS", "Maximum number of threads to use for parsing") do |t|
         self.options[:threads] = t.to_i if t.to_i > 0
-
       end
       opts.on("-h", "This information") do |h|
         puts opts
         exit 0
       end
     end.parse!(args)
-
     load_log args.shift
   end
 
   def load_log file=""
     raise ApacheLookupError, "No log file supplied" if(file.nil? or file == "")
+    @orig_file = file
+    @log_data = File.readlines(file).map {|line| line.chomp}
   end
 
   def lookup log_line=""
-    log_bits = log_line.split('-')
-    dns = Resolv.getname(log_bits[0].strip)
-    log_bits[0] = "#{dns} "
-    log_bits.join('-')
+    log_bits = log_line.split(" - - ")
+    dns = get_cache(log_bits[0].strip)
+    if !dns
+      dns = Resolv.getname(log_bits[0].strip)
+      FileUtils.touch CACHE_FILE if !File.exists?(CACHE_FILE)
+      cf = File.open(CACHE_FILE, "r+")
+      to_write = "#{log_bits[0].strip}|#{dns}|#{Time.now}"
+      cf.puts(to_write)
+      cf.close
+    end
+    log_bits[0] = "#{dns}"
+    log_bits.join(" - - ")
+  end
+
+  def resolve
+    @log_data.map!{|line| 
+      lookup(line)
+    }
+  end
+
+  def write
+    FileUtils.cp(@orig_file, "#{@orig_file}.orig")
+    File.open(@orig_file, "w") {|file|
+      @log_data.each {|line|
+        file.puts(line)
+      }
+    }
+  end
+
+  def self.run args=[]
+    al = ApacheLookup.new(args)
+    al.resolve
+    al.write
+  end
+
+  private
+
+  def get_cache(ip)
+    found_and_current = false
+    return found_and_current if !File.exists?(CACHE_FILE)
+    File.open(CACHE_FILE, "r") do |file|
+      file.each do |line|
+        bits = line.split("|")
+        if bits[0] == ip and ((Time.now - Time.parse(bits[2])) < CACHE_DECAY)
+          found_and_current = bits[1]
+        end
+      end
+    end
+    found_and_current
   end
 end
 
 class ApacheLookupError < Exception; end
-
-ApacheLookup.new(ARGV) if __FILE__ == $0
--- test/test_apache_lookup.rb
+++ test/test_apache_lookup.rb
@@ -1,6 +1,5 @@
 require 'test/unit'
 require 'apache_lookup'
-require 'fileutils'
 require 'rubygems'
 require 'flexmock/test_unit'
 
@@ -8,11 +7,14 @@ class TestApacheLookup < Test::Unit::TestCase
 
   def setup
     @dir = "#{FileUtils.pwd}/test"
+    @cache = File.dirname(__FILE__) + "/../lib/lookup_cache.txt"
+    @log_line = '75.119.201.189 - - [29/Apr/2009:16:07:44 -0700] "GET /favicon.ico HTTP/1.1" 200 1406'
+    @mock_log_line = 'www.foo.com - - [29/Apr/2009:16:07:44 -0700] "GET /favicon.ico HTTP/1.1" 200 1406'
     # should make a local my_logs.log to work with so future runs of tests don't get clobbered by the last run
     FileUtils.cp("#{@dir}/my_logs.log", "#{@dir}/test_my_logs.log")
 
     # should delete the cache log if it exists to not clobber future tests.
-    File.delete("#{@dir}/lookup_cache.log") if File.exists?("#{@dir}/lookup_cache.log")
+    File.delete(@cache) if File.exists?(@cache)
   end
 
   def teardown
@@ -27,12 +29,14 @@ class TestApacheLookup < Test::Unit::TestCase
   end
 
   def test_loads_log_file
-    flunk "must make sure it is able to read in the file"
+    # "must make sure it is able to read in the file"
+    al = ApacheLookup.new(["#{@dir}/test_my_logs.log"])
+    assert_equal 12, al.log_data.size
   end
 
-  def test_bad_threadcount_value
+  def test_handles_bad_threadcount_value
     # "for when a user specifies a number of threads, but does so badly: 'foo'"
-    al = ApacheLookup.new(%w(-t foo my_logs.log))
+    al = ApacheLookup.new(["-t", "foo", "#{@dir}/test_my_logs.log"])
     assert_equal 5, al.options[:threads]
   end
 
@@ -42,28 +46,74 @@ class TestApacheLookup < Test::Unit::TestCase
     dns = "www.foo.com"
     al = ApacheLookup.new(["#{@dir}/test_my_logs.log"])
     flexmock(Resolv).should_receive(:getname).with(ip_address).once.and_return(dns)
-    actual = al.lookup('75.119.201.189 - - [29/Apr/2009:16:07:44 -0700] "GET /favicon.ico HTTP/1.1" 200 1406')
-    expected = 'www.foo.com - - [29/Apr/2009:16:07:44 -0700] "GET /favicon.ico HTTP/1.1" 200 1406'
-    assert_equal actual, expected
+    assert_equal @mock_log_line, al.lookup(@log_line)
   end
 
   def test_caches_domain_name
-    flunk "use flexmock to make sure rsolv runs once, and then run again with same IP and make sure rsolv doesn't run the second time, but same result"
+    # "use flexmock to make sure resolv runs once, and then run again with same IP and make sure rsolv doesn't run the second time, but same result"
+    al = ApacheLookup.new(["#{@dir}/test_my_logs.log"])
+    flexmock(Resolv).should_receive(:getname => "www.foo.com").at_most.once
+    first = al.lookup(@log_line)
+    alter_cache(ApacheLookup::CACHE_FILE, :dns, "75.119.201.189")
+    second = al.lookup(@log_line)
+    expected = 'www.bar.com - - [29/Apr/2009:16:07:44 -0700] "GET /favicon.ico HTTP/1.1" 200 1406'
+    assert_equal expected, second
   end
 
   def test_cached_domain_name_ages_and_expires
-    flunk "use flexmock to rsolv the IP, then make the cache for the given name look more than one day old, then make sure rsolv is used again for same IP"
+    # "use flexmock to resolv the IP, then make the cache for the given name look more than one day old, then make sure rsolv is used again for same IP"
+    al = ApacheLookup.new(["#{@dir}/test_my_logs.log"])
+    flexmock(Resolv).should_receive(:getname => "www.foo.com").at_most.times(2).at_least.times(2)
+    first = al.lookup(@log_line)
+    alter_cache(@cache, :time, "75.119.201.189")
+    assert_equal @mock_log_line, al.lookup(@log_line)
   end
 
   def test_log_file_is_safely_written_to
-    flunk "make sure the original log is preserved somehow, like appending a .orig to it, and that the original filename still exists with the new data."
+    #flunk "make sure the original log is preserved somehow, like appending a .orig to it, and that the original filename still exists with the new data."
+    # this looks like it would be equivalent to a ApacheLookup#run call...
+    al = ApacheLookup.new(["#{@dir}/test_my_logs.log"])
+    flexmock(Resolv).should_receive(:getname => "www.foo.com")
+    al.resolve
+    al.write
+    assert File.exists?("#{@dir}/test_my_logs.log.orig")
+    assert !FileUtils.identical?("#{@dir}/test_my_logs.log", "#{@dir}/test_my_logs.log.orig")
+    assert FileUtils.identical?("#{@dir}/test_my_logs.log.orig", "#{@dir}/my_logs.log")
   end
 
   def test_log_order_is_maintained
-    flunk "make sure that the order (the date/time stamp) is still in ascending order, or maybe test to make sure it still matches what's in the original file"
+    #flunk "make sure that the order (the date/time stamp) is still in ascending order, or maybe test to make sure it still matches what's in the original file"
+    al = ApacheLookup.new(["#{@dir}/test_my_logs.log"])
+    flexmock(Resolv).should_receive(:getname => "www.foo.com")
+    al.resolve
+    al.write
+    orig = File.readlines("#{@dir}/test_my_logs.log.orig").map{|line| line.chomp}
+    resolved = File.readlines("#{@dir}/test_my_logs.log").map{|line| line.chomp}
+    (0...orig.size).each do |i|
+      actual = orig[i].split(" - - ")
+      expected = resolved[i].split(" - - ")
+      assert_equal orig[i].split(" - - ")[1], resolved[i].split(" - - ")[1]
+    end
   end
 
-  def test_thread_management_is_working
-    flunk "how the heck do we test this?"
+  def alter_cache(cache_file, value, lookup)
+    cf = File.readlines(cache_file)
+    cf = cf.map {|l|
+      if l =~ /#{lookup}/
+        line_bits = l.split("|")
+        line_bits[1] = "www.bar.com"
+        if value == :time
+          line_bits[2] = "#{Time.now-90_000}"
+        end
+        line_bits.join("|")
+      else 
+        l
+      end
+    }
+    File.open(cache_file, "w") do |file|
+      cf.each do |line|
+        file.write(line)
+      end
+    end
   end
 end
